23bd2e6a9bcfa185d1447b155ccffc1ee69a29fe
Jin <126914439+jinovative@users.noreply.github.com>
2023-11-30 02:04:37 -0500

    Update mybooks.py
    organize the GUI setup code into a single function, setup_gui, within mybooks.py file.

--- mybooks.py
+++ mybooks.py
@@ -100,71 +100,73 @@ def on_closing():
         root.destroy()
         del dd
 
-root = Tk()  # Creates application window
-root.title("My Books Database Application") # Adds a title to application window
-root.configure(background="light green")  # Add background color to application window
-root.geometry("1800x550")  # Sets a size for application window
-root.resizable(width=False,height=False) # Prevents the application window from resizing
+def setup_gui(root):
+    """
+    Sets up the GUI components of the application.
+    This includes labels, entry widgets, buttons, listbox, and scrollbar.
+    """
+    # Window Configuration
+    root.title("My Books Database Application")
+    root.configure(background="light green")
+    root.geometry("1100x550")
+    root.resizable(width=False, height=False)
 
-# Create Labels and entry widgets
-title_label = ttk.Label(root, text="Title", background="light green", font=("TkDefaultFont", 16))
-title_label.grid(row=0, column=0, sticky=W)
-title_text = StringVar()
-title_entry = ttk.Entry(root, width=24, textvariable=title_text)
-title_entry.grid(row=0, column=1, sticky=W)
+    # Create Labels and Entry Widgets
+    title_label = ttk.Label(root, text="Title", background="light green", font=("TkDefaultFont", 16))
+    title_label.grid(row=0, column=0, sticky=W)
+    title_text = StringVar()
+    title_entry = ttk.Entry(root, width=24, textvariable=title_text)
+    title_entry.grid(row=0, column=1, sticky=W)
 
-author_label = ttk.Label(root, text="Author", background="light green", font=("TkDefaultFont", 16))
-author_label.grid(row=0, column=2, sticky=E)
-author_text = StringVar()
-author_entry = ttk.Entry(root, width=24, textvariable=author_text)
-author_entry.grid(row=0, column=3, sticky=W)
+    author_label = ttk.Label(root, text="Author", background="light green", font=("TkDefaultFont", 16))
+    author_label.grid(row=0, column=2, sticky=E)
+    author_text = StringVar()
+    author_entry = ttk.Entry(root, width=24, textvariable=author_text)
+    author_entry.grid(row=0, column=3, sticky=W)
 
-isbn_label = ttk.Label(root, text="ISBN", background="light green", font=("TkDefaultFont", 14))
-isbn_label.grid(row=0, column=4, sticky=E)
-isbn_text = StringVar()
-isbn_entry = ttk.Entry(root, width=24, textvariable=isbn_text)
-isbn_entry.grid(row=0, column=5, sticky=W)
+    isbn_label = ttk.Label(root, text="ISBN", background="light green", font=("TkDefaultFont", 14))
+    isbn_label.grid(row=0, column=4, sticky=E)
+    isbn_text = StringVar()
+    isbn_entry = ttk.Entry(root, width=24, textvariable=isbn_text)
+    isbn_entry.grid(row=0, column=5, sticky=W)
+    
+    # Add a Button to Insert Inputs into Database
+    add_btn = Button(root, text="Add Book", bg="blue", fg="black", font="helvetica 10 bold", command=add_book)
+    add_btn.grid(row=0, column=6, sticky=W)
 
+    # Listbox to Display Data from Database
+    list_bx = Listbox(root, height=16, width=40, font="helvetica 13", bg="light blue")
+    list_bx.grid(row=3, column=1, columnspan=14, sticky=W + E, pady=40, padx=15)
+    list_bx.bind('<<ListboxSelect>>', get_selected_row)
 
-ratings_label = ttk.Label(root, text="Ratings", background="light green", font=("TkDefaultFont", 16))
-ratings_label.grid(row=0, column=6, sticky=E)
-ratings_text = StringVar()
-ratings_entry = ttk.Entry(root, width=24, textvariable=ratings_text)
-ratings_entry.grid(row=0, column=7, sticky=W)
+    # Scrollbar for the Listbox
+    scroll_bar = Scrollbar(root)
+    scroll_bar.grid(row=1, column=8, rowspan=14, sticky=W)
+    list_bx.configure(yscrollcommand=scroll_bar.set)
+    scroll_bar.configure(command=list_bx.yview)
 
-# Add a button to insert inputs into database
+    # Additional Button Widgets
+    modify_btn = Button(root, text="Modify Record",bg="purple",fg="black",font="helvetica 10 bold",command=update_records)
+    modify_btn.grid(row=15, column=4)
 
-add_btn = Button(root, text="Add Book",bg="blue",fg="black",font="helvetica 10 bold",command=add_book)
-add_btn.grid(row=0, column=8, sticky=W)
+    delete_btn = Button(root, text="Delete Record",bg="red",fg="black",font="helvetica 10 bold",command=delete_records)
+    delete_btn.grid(row=15, column=5)
 
-# Add  a listbox  to display data from database
-list_bx = Listbox(root,height=16,width=40,font="helvetica 13",bg="light blue")
-list_bx.grid(row=3,column=1, columnspan=14,sticky=W + E,pady=40,padx=15)
-list_bx.bind('<<ListboxSelect>>',get_selected_row)
+    view_btn = Button(root, text="View all records",bg="black",fg="black",font="helvetica 10 bold",command=view_records)
+    view_btn.grid(row=15, column=1)#, sticky=tk.N)
 
-# Add scrollbar to enable scrolling
-scroll_bar = Scrollbar(root)
-scroll_bar.grid(row=1,column=8, rowspan=14,sticky=W )
+    clear_btn = Button(root, text="Clear Screen",bg="maroon",fg="black",font="helvetica 10 bold",command=clear_screen)
+    clear_btn.grid(row=15, column=2)#, sticky=tk.W)
 
-list_bx.configure(yscrollcommand=scroll_bar.set) # Enables vetical scrolling
-scroll_bar.configure(command=list_bx.yview)
+    exit_btn = Button(root, text="Exit  Application",bg="blue",fg="black",font="helvetica 10 bold",command=root.destroy)
+    exit_btn.grid(row=15, column=3)
 
-# Add more Button Widgets
+# Main function to run the application
+def main():
+    root = Tk()
+    setup_gui(root)
+    root.mainloop()
 
-modify_btn = Button(root, text="Modify Record",bg="purple",fg="black",font="helvetica 10 bold",command=update_records)
-modify_btn.grid(row=15, column=4)
+if __name__ == "__main__":
+    main()
 
-delete_btn = Button(root, text="Delete Record",bg="red",fg="black",font="helvetica 10 bold",command=delete_records)
-delete_btn.grid(row=15, column=5)
-
-view_btn = Button(root, text="View all records",bg="black",fg="black",font="helvetica 10 bold",command=view_records)
-view_btn.grid(row=15, column=1)#, sticky=tk.N)
-
-clear_btn = Button(root, text="Clear Screen",bg="maroon",fg="black",font="helvetica 10 bold",command=clear_screen)
-clear_btn.grid(row=15, column=2)#, sticky=tk.W)
-
-exit_btn = Button(root, text="Exit  Application",bg="blue",fg="black",font="helvetica 10 bold",command=root.destroy)
-exit_btn.grid(row=15, column=3)
-
-
-root.mainloop()  # Runs the application until exit
